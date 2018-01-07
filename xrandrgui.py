f77a9078fe7f4592f83a5534825c9e24bc09d2d2
joxborrow <jonathan.todd.oxborrow@gmail.com>
2018-01-06 20:55:58 -0600

    Refined the GUIrandrgui.py

--- xrandrgui.py
+++ xrandrgui.py
@@ -6,17 +6,34 @@ This file provides a simple way to change the settings on a linux machine using 
 provides access to only the most basic settings.
 '''
 from tkinter import *
-
+# Est ablish the base window
 root = Tk()
-root.geometry("370x200+30+30")
-w = Label(root, text="Brightness:")
-w.place(x=10, y=1,  width=80,  height=20)
-w = Scale(root, from_=0, to=200, orient=HORIZONTAL)
-w.pack()
-w = Scale(root, from_=0, to=200, orient=HORIZONTAL)
-w.pack()
-w = Scale(root, from_=0, to=200, orient=HORIZONTAL)
-w.pack()
-w = Scale(root, from_=0, to=200, orient=HORIZONTAL)
-w.pack()
+root.title("xrandrgui")
+
+# Create the widgets
+lbl_bright = Label(root, text="Brightness:")
+scl_bright = Scale(root, from_=0, to=200, orient=HORIZONTAL,  length=400)
+lbl_space = Label(root,  text=" ")
+lbl_gamma = Label(root,  text="Gamma:")
+lbl_r = Label(root,  text="R:")
+scl_r = Scale(root, from_=0, to=200, orient=HORIZONTAL, length=400)
+lbl_g = Label(root, text="G:")
+scl_g = Scale(root, from_=0, to=200, orient=HORIZONTAL,  length=400)
+lbl_b = Label(root,  text="B:")
+scl_b = Scale(root, from_=0, to=200, orient=HORIZONTAL,  length=400)
+
+# Place the widgets using grid
+lbl_bright.grid(row=0,  column=0, sticky="SE")
+scl_bright.grid(row=0,  column=1)
+lbl_space.grid(row=1,  column=0)
+lbl_gamma.grid(row=2,  column=0,  sticky="SE")
+lbl_r.grid(row=3,  column=0,  sticky="SE")
+scl_r.grid(row=3,  column=1)
+lbl_g.grid(row=4,  column=0,  sticky="SE")
+scl_g.grid(row=4,  column=1)
+lbl_b.grid(row=5,  column=0,  sticky="SE")
+scl_b.grid(row=5,  column=1)
+
+
+# Loop
 root.mainloop()
