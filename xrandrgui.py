f3a44fd901f0bbf0c927d41575f3c6b1bd937110
joxborrow <jonathan.todd.oxborrow@gmail.com>
2018-01-23 21:11:05 -0600

    Started adding resolution selector.

--- xrandrgui.py
+++ xrandrgui.py
@@ -23,47 +23,55 @@ class xrandrgui:
         self.optionmenu1 = OptionMenu(master, self.op1, *output_options)
         self.optionmenu1.grid(row=0, column=1, sticky="W")
 
+        # Resolution
+        self.label01 = Label(master, text="Resolution")
+        self.label01.grid(row=1, column=0)
+        resolution_options = ["test1", "test2"]
+        self.op2 = StringVar(master)
+        self.op2.set(resolution_options[0])
+        self.optionmenu2 = OptionMenu(master, self.op2, *resolution_options)
+        self.optionmenu2.grid(row=1, column=1, sticky="W")
+
         # Brightness
         self.label1 = Label(master, text="Brightness")
-        self.label1.grid(row=1, column=0, sticky="SE")
+        self.label1.grid(row=2, column=0, sticky="SE")
         self.sc_brightness = Scale(master, from_=0, to_=5, length=300,
                                    orient=HORIZONTAL, resolution=.05,
                                    command=self.run_prog)
-        self.sc_brightness.grid(row=1, column=1)
+        self.sc_brightness.grid(row=2, column=1)
 
         # Gamma
         self.label2 = Label(master, text="Gamma")
-        self.label2.grid(row=2, column=0, sticky="S")
+        self.label2.grid(row=3, column=0, sticky="S")
 
         # R
         self.label3 = Label(master, text="R ")
-        self.label3.grid(row=3, column=0, sticky="SE")
+        self.label3.grid(row=4, column=0, sticky="SE")
         self.sc_r = Scale(master, from_=0, to_=5, length=300,
                           orient=HORIZONTAL, resolution=.05,
                           command=self.run_prog)
-        self.sc_r.grid(row=3, column=1)
+        self.sc_r.grid(row=4, column=1)
 
         # G
         self.label4 = Label(master, text="G ")
-        self.label4.grid(row=4, column=0, sticky="SE")
+        self.label4.grid(row=5, column=0, sticky="SE")
         self.sc_g = Scale(master, from_=0, to_=5, length=300,
                           orient=HORIZONTAL, resolution=.05,
                           command=self.run_prog)
-        self.sc_g.grid(row=4, column=1)
+        self.sc_g.grid(row=5, column=1)
 
         # B
         self.label5 = Label(master, text="B ")
-        self.label5.grid(row=5, column=0, sticky="SE")
+        self.label5.grid(row=6, column=0, sticky="SE")
         self.sc_b = Scale(master, from_=0, to_=5, length=300,
                           orient=HORIZONTAL, resolution=.05,
                           command=self.run_prog)
-        self.sc_b.grid(row=5, column=1)
-
-        # Command Label
-        self.label6 = Label(master,)
+        self.sc_b.grid(row=6, column=1)
 
         # Initialize control values
         self.sc_brightness.set(1)
+        #gamma = subprocess.getoutput("xrandr --verbose | awk '/Gamma/ {print $2}'").split(":")
+        #print(gamma)
         self.sc_r.set(1)
         self.sc_g.set(1)
         self.sc_b.set(1)
