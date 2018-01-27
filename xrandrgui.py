f960efb4bb69f7f97aa2782c74c5cea2e070a18b
joxborrow <jonathan.todd.oxborrow@gmail.com>
2018-01-26 20:21:08 -0600

    Refined some of the layout.

--- xrandrgui.py
+++ xrandrgui.py
@@ -14,63 +14,61 @@ class xrandrgui:
         # Get attached monitors
         monitors = subprocess.getoutput("xrandr --query | awk '/ connected / {print $1}'")
 
-        # Frame
-        frame1=Frame(self.master, width=370, height=230)
-        frame1.grid(columnspan=3, rowspan=7, padx=5, pady=5)
-
         # Output
         self.label0 = Label(master, text="Output")
-        self.label0.grid(row=0, column=0)
+        self.label0.grid(row=0, column=0, padx=2, pady=2, sticky="E")
         output_options = tuple(monitors.split("\n"))
         self.op1 = StringVar(master)
         self.op1.set(output_options[0])
         self.optionmenu1 = OptionMenu(master, self.op1, *output_options)
-        self.optionmenu1.grid(row=0, column=1, sticky="W")
+        self.optionmenu1.config(width=10)
+        self.optionmenu1.grid(row=0, column=1, sticky="W", padx=2, pady=2)
 
         # Resolution
         self.label01 = Label(master, text="Resolution")
-        self.label01.grid(row=1, column=0)
+        self.label01.grid(row=1, column=0, padx=2, pady=2, sticky=E)
         resolution_options = ["test1", "test2"]
         self.op2 = StringVar(master)
         self.op2.set(resolution_options[0])
         self.optionmenu2 = OptionMenu(master, self.op2, *resolution_options)
-        self.optionmenu2.grid(row=1, column=1, sticky="W")
+        self.optionmenu2.config(width=10)
+        self.optionmenu2.grid(row=1, column=1, sticky="W", padx=2, pady=2)
 
         # Brightness
         self.label1 = Label(master, text="Brightness")
-        self.label1.grid(row=2, column=0, sticky="SE")
+        self.label1.grid(row=2, column=0, sticky="SE", padx=2, pady=2)
         self.sc_brightness = Scale(master, from_=0, to_=5, length=300,
                                    orient=HORIZONTAL, resolution=.05,
                                    command=self.run_prog)
-        self.sc_brightness.grid(row=2, column=1)
+        self.sc_brightness.grid(row=2, column=1, padx=2, pady=2)
 
         # Gamma
         self.label2 = Label(master, text="Gamma")
-        self.label2.grid(row=3, column=0, sticky="S")
+        self.label2.grid(row=3, column=0, sticky="SE", padx=2, pady=2)
 
         # R
         self.label3 = Label(master, text="R ")
-        self.label3.grid(row=4, column=0, sticky="SE")
+        self.label3.grid(row=4, column=0, sticky="SE", padx=2, pady=2)
         self.sc_r = Scale(master, from_=0, to_=5, length=300,
                           orient=HORIZONTAL, resolution=.05,
                           command=self.run_prog)
-        self.sc_r.grid(row=4, column=1)
+        self.sc_r.grid(row=4, column=1, padx=2, pady=2)
 
         # G
         self.label4 = Label(master, text="G ")
-        self.label4.grid(row=5, column=0, sticky="SE")
+        self.label4.grid(row=5, column=0, sticky="SE", padx=2, pady=2)
         self.sc_g = Scale(master, from_=0, to_=5, length=300,
                           orient=HORIZONTAL, resolution=.05,
                           command=self.run_prog)
-        self.sc_g.grid(row=5, column=1)
+        self.sc_g.grid(row=5, column=1, padx=2, pady=2)
 
         # B
         self.label5 = Label(master, text="B ")
-        self.label5.grid(row=6, column=0, sticky="SE")
+        self.label5.grid(row=6, column=0, sticky="SE", padx=2, pady=2)
         self.sc_b = Scale(master, from_=0, to_=5, length=300,
                           orient=HORIZONTAL, resolution=.05,
                           command=self.run_prog)
-        self.sc_b.grid(row=6, column=1)
+        self.sc_b.grid(row=6, column=1, padx=2, pady=2)
 
         # Initialize control values
         self.sc_brightness.set(1)
